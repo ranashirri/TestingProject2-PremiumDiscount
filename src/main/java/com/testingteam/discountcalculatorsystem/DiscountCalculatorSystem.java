f216c76220c3ede0e38a954b4930e5025791a3ae
DELL <rana.shirri4@gmail.com>
2026-05-10 23:20:05 +0300

    system files created

---
+++ src/main/java/com/testingteam/discountcalculatorsystem/DiscountCalculatorSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testingteam.discountcalculatorsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class DiscountCalculatorSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

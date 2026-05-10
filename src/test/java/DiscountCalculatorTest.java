7d6645061daeec3b1b14bdf47fd363fc3982693d
DELL <rana.shirri4@gmail.com>
2026-05-10 23:32:07 +0300

    create classes and add dependencies

---
+++ src/test/java/DiscountCalculatorTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+/**
+ *
+ * @author DELL
+ */
+public class DiscountCalculatorTest {
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
